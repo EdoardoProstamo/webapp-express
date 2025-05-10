a1e141495af30e5ac9143613e89e5a37754c2870
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-05-10 19:41:57 +0200

    feat: variabili d’ambiente

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 // middlewares
 const notFound = require('./middlewares/notFound');
 const handleErrors = require('./middlewares/handleErrors');
--- data/db.js
+++ data/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
 
-    host: 'localhost',
-    user: 'root',
-    password: 'Jsbuuijlsj46772',
-    database: 'movies'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 
 });
 

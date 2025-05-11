63b99a02e12981e67a3a550e6f9b33187373e1c6
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-05-11 12:33:45 +0200

    feat: rotte index & show

--- controllers/controllerMovie.js
+++ controllers/controllerMovie.js
@@ -2,13 +2,64 @@ const connection = require('../data/db');
 
 function index(req, res) {
 
-    res.send('Lista film');
+    const sql = 'SELECT * FROM movies.movies;';
+
+    connection.query(sql, (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                errorMessage: "Errore di connessione al Database."
+            })
+        };
+
+        res.json(results);
+
+    });
 
 };
 
 function show(req, res) {
 
-    res.send('Mostra film');
+    const sql = `SELECT * FROM movies.movies WHERE id = ?;`;
+
+    const { id } = req.params;
+
+    connection.query(sql, [id], (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                errorMessage: "Errore di connessione al Database."
+            });
+        };
+
+        if (err) {
+            return res.status(404).json({
+                errorMessage: "Elemento non trovato. Error 404!"
+            });
+        };
+
+        const moviesList = res.json(results[0]);
+
+        // recensioni in show
+        const sql = "SELECT * FROM movies.reviews WHERE movie_id=?;";
+
+        connection.query(sql, [id], (err, results) => {
+
+            if (err) {
+                return res.status(500).json({
+                    errorMessage: "Errore di connessione al Database."
+                });
+            };
+            if (err) {
+                return res.status(404).json({
+                    errorMessage: "Elemento non trovato. Error 404!"
+                });
+            };
+
+            moviesList.reviews = results;
+            res.json(moviesList);
+        });
+    });
 
 };
 

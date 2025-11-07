be315494d4670c1ab89654274eb3113568fd7580
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-11-07 17:55:22 +0100

    fix: media voto recensioni

--- controllers/controllerMovie.js
+++ controllers/controllerMovie.js
@@ -1,121 +1,86 @@
 const connection = require('../data/db');
 const slugify = require('slugify');
 
-// lista film
+// LISTA FILM
 function index(req, res) {
-
     const { ricerca } = req.query;
 
-    // richiesta, con unione delle tabelle che ci servono (tramite JOIN), solo di determinati campi all'interno del DB
     let sql = `
-    SELECT movies.*, ROUND(AVG(reviews.vote), 2) AS media_voto_recensioni 
-    FROM movies.movies 
-    LEFT JOIN movies.reviews 
-    ON movie_id = reviews.movie_id `;
+    SELECT m.*, IFNULL(rv.media_voto, 0) AS media_voto_recensioni
+    FROM movies.movies m
+    LEFT JOIN (
+        SELECT movie_id, ROUND(AVG(vote), 2) AS media_voto
+        FROM movies.reviews
+        GROUP BY movie_id
+    ) rv ON m.id = rv.movie_id
+    `;
 
+    const params = [];
     if (ricerca) {
-
-        sql += `WHERE title LIKE "%${ricerca}%" OR director LIKE "%${ricerca}%" OR abstract LIKE "%${ricerca}%" OR release_year LIKE "%${ricerca}%" OR genre LIKE "%${ricerca}%"`
+        sql += ` WHERE title LIKE ? OR director LIKE ? OR abstract LIKE ? OR release_year LIKE ? OR genre LIKE ?`;
+        params.push(`%${ricerca}%`, `%${ricerca}%`, `%${ricerca}%`, `%${ricerca}%`, `%${ricerca}%`);
     }
 
-    sql += 'GROUP BY movies.id'
-
-    // connessione al DB per ricevere le informazioni dei film
-    connection.query(sql, (err, results) => {
-
-
+    connection.query(sql, params, (err, results) => {
         if (err) {
-            return res.status(500).json({
-                errorMessage: "Errore di connessione al Database."
-            })
-        };
+            return res.status(500).json({ errorMessage: "Errore di connessione al Database." });
+        }
 
         res.json(results.map(result => ({
             ...result,
             imagePath: process.env.IMG + result.image
         })));
-
     });
+}
 
-};
-
-// singolo film
+// SINGOLO FILM
 function show(req, res) {
-    // dettaglio single movie in show
-    const sql = `
-    SELECT movies.*, ROUND(AVG(reviews.vote), 2) AS media_voto_recensioni 
-    FROM movies.movies 
-    LEFT JOIN movies.reviews 
-    ON movie_id = reviews.movie_id 
-    WHERE movies.slug=?`;
-
     const { slug } = req.params;
 
+    const sql = `
+    SELECT m.*, IFNULL(rv.media_voto, 0) AS media_voto_recensioni
+    FROM movies.movies m
+    LEFT JOIN (
+        SELECT movie_id, ROUND(AVG(vote), 2) AS media_voto
+        FROM movies.reviews
+        GROUP BY movie_id
+    ) rv ON m.id = rv.movie_id
+    WHERE m.slug = ?
+    `;
+
     connection.query(sql, [slug], (err, results) => {
+        if (err) return res.status(500).json({ errorMessage: "Errore di connessione al Database." });
+        if (!results.length) return res.status(404).json({ errorMessage: "Film non trovato" });
 
-        if (err) {
-            return res.status(500).json({
-                errorMessage: "Errore di connessione al Database."
-            });
-        };
-
-        if (err) {
-            return res.status(404).json({
-                errorMessage: "Elemento non trovato. Error 404!"
-            });
-        };
-
-        const risultatoCorrente = results[0];
-
-        const moviesList = {
+        const movie = {
             ...results[0],
             imagePath: process.env.IMG + results[0].image
         };
 
-        // recensioni in show
-        const sql = "SELECT * FROM movies.reviews WHERE movie_id=?;";
+        // Recupero recensioni
+        const sqlReviews = "SELECT * FROM movies.reviews WHERE movie_id = ?";
+        connection.query(sqlReviews, [movie.id], (err, reviews) => {
+            if (err) return res.status(500).json({ errorMessage: "Errore di connessione al Database." });
 
-        connection.query(sql, [risultatoCorrente.id], (err, results) => {
-
-            if (err) {
-                return res.status(500).json({
-                    errorMessage: "Errore di connessione al Database."
-                });
-            };
-            if (err) {
-                return res.status(404).json({
-                    errorMessage: "Elemento non trovato. Error 404!"
-                });
-            };
-
-            moviesList.reviews = results;
-            res.json(moviesList);
+            movie.reviews = reviews;
+            res.json(movie);
         });
     });
+}
 
-};
-
-// recensioni
+// CREAZIONE RECENSIONE
 function store(req, res) {
-
-    // id del film del quale viene creata la nuova recensione
     const { slug } = req.params;
-
-    // valori della nuova recensione che voglio siano utilizzati
     const { name, vote, text } = req.body;
 
     const sqlMovie = "SELECT id FROM movies.movies WHERE slug = ?";
-
-
     connection.query(sqlMovie, [slug], (err, results) => {
         if (err) return res.status(500).json({ errorMessage: "Errore di connessione al Database." });
         if (!results.length) return res.status(404).json({ errorMessage: "Film non trovato" });
 
         const movieId = results[0].id;
-
-        // inserimento recensione
         const sqlReview = "INSERT INTO reviews (movie_id, name, vote, text) VALUES (?, ?, ?, ?)";
-        connection.query(sqlReview, [movieId, name, vote, text], (err, results) => {
+        connection.query(sqlReview, [movieId, name, vote, text], (err) => {
             if (err) return res.status(500).json({ errorMessage: "Errore di connessione al Database." });
 
             res.json({
@@ -127,39 +92,20 @@ function store(req, res) {
             });
         });
     });
-};
+}
 
-// nuovo film
+// CREAZIONE NUOVO FILM
 function storeMovie(req, res) {
-
-    // valori del nuovo film che voglio siano utilizzati
     const { title, director, genre, release_year, abstract } = req.body;
-
     const imageName = req.file.filename;
-
-    // comando utilizzato nel database (richiesta inserimento nuovi elementi, in questo caso)
-    const sql = `INSERT INTO movies.movies (title, director, genre, release_year, abstract, image, slug) VALUES (?, ?, ?, ?, ?, ?, ?);`
-
-    // slug
     const slug = slugify(title);
 
-    // connessione: eventuali errori e risposta alla richiesta
-    connection.query(sql, [title, director, genre, release_year, abstract, imageName, slug], (err, results) => {
-        if (err) {
-            return res.status(500).json({
-                errorMessage: "Errore di connessione al Database."
-            });
-        };
-
-        if (err) {
-            return res.status(404).json({
-                errorMessage: "Elemento non trovato. Error 404!"
-            });
-        };
+    const sql = `INSERT INTO movies.movies (title, director, genre, release_year, abstract, image, slug) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+    connection.query(sql, [title, director, genre, release_year, abstract, imageName, slug], (err) => {
+        if (err) return res.status(500).json({ errorMessage: "Errore di connessione al Database." });
 
         res.json({ message: 'Nuovo film aggiunto' });
     });
-
-};
+}
 
 module.exports = { index, show, store, storeMovie };
